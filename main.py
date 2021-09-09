2944507a6cf520de3abf181afbbd07a5179bea83
Ryan Smith <ryan.smith@ucdconnect.ie>
2021-09-09 14:32:55 +0100

    working grayscale (write first img)

--- main.py
+++ main.py
@@ -3,30 +3,49 @@ Program to interact with GenICam devices. Stores images in a H5 dataset.
 """
 import numpy as np
 import h5py
+import matplotlib.pyplot as plt
 
 from harvesters.core import Harvester
 from harvesters.util.pfnc import mono_location_formats, bgr_formats
+
+###############################################################################
+#                                   To Do
+# Dont write first image to h5 file. init with empty shape (1000, h, w, p)
+# convert image to save as rgb func
+# save images as grayscale
+# save correctly in h5 files s
+###############################################################################
 
 
 class H5ImageDataset():
     # readwrite : 'w' for writing new dataset
     #             'r' for reading images
     #             'a' for appending to existing dataset
-    def __init__(self, h5path, component=None, readwrite='w'):
+    def __init__(self, h5path, component=None, readwrite='w', verbose=True):
         if readwrite == 'w':
             image = self.first_image(component)
-            print(self.data_format)
-            h5file = h5py.File(h5path, readwrite)
-            dataset = h5file.create_dataset('images', data=image, dtype=np.int,
-                                            maxshape=(None,
-                                                      self.height,
-                                                      self.width,
-                                                      self.pixel))
-            dataset.attrs['format'] = self.data_format,
+            self.h5file = h5py.File(h5path, readwrite)
+            dataset = self.h5file.create_dataset('images',
+                                                 shape=(1,
+                                                        self.height,
+                                                        self.width,
+                                                        self.pixel),
+                                                 data=image,
+                                                 dtype=np.int8,
+                                                 maxshape=(None,
+                                                           self.height,
+                                                           self.width,
+                                                           self.pixel))
+            dataset.attrs.create('format', self.data_format)
             dataset.attrs['height'] = self.height,
             dataset.attrs['width'] = self.width,
             dataset.attrs['pixel'] = self.pixel
-            h5file.close()
+            self.h5file.close()
+            if verbose:
+                print(f"Writing to path: {h5path}\n",
+                      f"Using data format: {self.data_format}\n",
+                      f"Image shape: {image.shape}")
+
             self.dataset = h5py.File(h5path, 'a')['images']
         elif readwrite == 'r':
             self.dataset = h5py.File(h5path, 'r')['images']
@@ -60,34 +79,51 @@ class H5ImageDataset():
         self.data_format = component.data_format
         self.pixel = int(component.num_components_per_pixel)
 
+    def read(self, index='return all images'):
+        if index=='return all images':
+            return self.dataset
+        else:
+            image = self.dataset[index]
+            return image
+
     def close(self):
-        self.dataset.close()
+        self.h5file.close()
 
 
-if __name__ == "__main__":
-
-    H5PATH = './test.h5'
-    MAX_IMAGES = 10
-
+def collect_images(h5path,
+                   cti_file,
+                   max_images=100
+                   ):
     with Harvester() as h:
-        # h.add_cti_file('/opt/cvb-13.03.003/drivers/genicam/libGevTL.cti')
-        h.add_cti_file('/opt/mvIMPACT_Acquire/lib/x86_64/mvGenTLProducer.cti')
+        h.add_cti_file(cti_file)
         h.update_device_info_list()
         with h.create_image_acquirer(0) as ia:
             ia.start_image_acquisition()
             count = 0
-            while count < MAX_IMAGES:
+            while count < max_images:
                 with ia.fetch_buffer() as buffer:
                     payload = buffer.payload
                     component = payload.components[0]
                     if count == 0:
-                        dataset = H5ImageDataset(H5PATH, component=component)
+                        dataset = H5ImageDataset(h5path, component=component)
                     else:
                         dataset.write_image(component)
                     count += 1
     dataset.close()
 
+
+if __name__ == "__main__":
+
+    H5PATH = './test.h5'
+    MAX_IMAGES = 100
+    CTI_FILE = '/opt/mvIMPACT_Acquire/lib/x86_64/mvGenTLProducer.cti'
+    # '/opt/cvb-13.03.003/drivers/genicam/libGevTL.cti'
+
+    #collect_images(H5PATH, CTI_FILE, MAX_IMAGES)
+
     dataset = H5ImageDataset(H5PATH, readwrite='r')
-    images = dataset['images']
+    images = dataset.read()
     img1 = images[0]
     print(img1.shape)
+    plt.imshow(img1, 'gray')
+    plt.show()
