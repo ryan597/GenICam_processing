998441a5fe2b449c40e84d3410e6d23ae5334a94
ryan597 <ryan.smith@ucdconnect.ie>
2022-01-24 13:38:07 +0000

    relative path from build

--- trigger.sh
+++ trigger.sh
@@ -5,6 +5,6 @@ numImgs=100
 acquisitionMode=Continuous  # SingleFrame
 pixelFormat=RGBx888Packed  # Mono8
 
-mkdir data/$imageDir
+mkdir ../data/$imageDir
 
-build/main $numImages $imageDir $acquisitionMode $pixelFormat |& tee logs/logging.txt
+build/main $numImages ../data/$imageDir $acquisitionMode $pixelFormat |& tee logs/logging.txt
