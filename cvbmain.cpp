a33fcde2c8aa07e96160bc147caf5cc70818f24c
ryan597 <ryan.smith@ucdconnect.ie>
2022-01-31 17:12:40 +0000

    change to buffer request and save later

--- cvbmain.cpp
+++ cvbmain.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+//#include <memory>
+#include <ctime>
 
-//#include <cvb/image.hpp>
+#include <cvb/image.hpp>
 #include <cvb/device_factory.hpp>
 //#include <cvb/utilities/system_info.hpp>
 #include <cvb/driver/stream.hpp>
@@ -9,12 +11,33 @@
 #include "cvbconfig.hpp"
 #include "videoprocessing.cpp"
 
+
 auto argsHelper() -> void
 {
-    std::cout << "Usage: $ ./main MAX_IMAGES IMAGE_DIRECTORY ACQUISITION_MODE PIXEL_FORMAT\n"
-              << "Example: ./main 1000 data/test Continuous Mono8\n";
+    std::cout << "Usage: $ ./main MAX_IMAGES IMAGE_DIRECTORY (BUFFERS WIDTH HEIGHT)\n"
+              << "Example: ./main 1000 data/test (100 2560 2048)\n";
 }
 
+auto findDevice() -> Cvb::DevicePtr
+{
+    /*
+     * Discover all devices connected, exits if none available
+     */
+    auto deviceList = Cvb::DeviceFactory::Discover(Cvb::DiscoverFlags::IgnoreVins, std::chrono::seconds(5));
+    if (deviceList.empty())
+    {
+        std::cout << "No device available... Program exit.\n";
+        exit(1);
+    }
+    auto device = Cvb::DeviceFactory::Open(deviceList[0].AccessToken());
+    std::cout << "Device open\n";
+    if (device->StreamCount() == 0)
+    {
+        std::cout << "No stream available... Program exit.\n";
+        exit(1);
+    }
+    return device;
+}
 
 auto main(int argc, char** argv) -> int
 {
@@ -27,63 +50,64 @@ auto main(int argc, char** argv) -> int
     // Read config params
     const int numImages = std::atoi(argv[1]);
     std::string imageDir = (argc > 2) ? argv[2] : "data";
-    //int width{};  // For reducing the AOI and thus resolution
-    //int height{};
+    const int buffers = (argc > 3) ? std::atoi(argv[3]) : 100;
+    const int width = (argc > 4) ? std::atoi(argv[4]) : 2560;  // For reducing the AOI and thus resolution
+    const int height = (argc > 5) ? std::atoi(argv[5]) : 2048;
 
     std::cout << "Initialising device...\n";
+    auto device = findDevice();
 
-    // open a device
-    auto deviceList = Cvb::DeviceFactory::Discover(Cvb::DiscoverFlags::IgnoreVins, std::chrono::seconds(5));
-    if (deviceList.empty())
-    {
-        std::cout << "No device available... Program exit.\n";
-        return 1;
-    }
+    // Set to software trigger to fill buffers
+    auto deviceNodeMap = device->NodeMap(CVB_LIT("Device"));
+    auto triggerMode = deviceNodeMap->Node<Cvb::EnumerationNode>("TriggerMode");
+    auto triggerSource = deviceNodeMap->Node<Cvb::EnumerationNode>("TriggerSource");
+    auto triggerSoftware = deviceNodeMap->Node<Cvb::CommandNode>("TriggerSoftware");
 
-    auto device = Cvb::DeviceFactory::Open(deviceList[0].AccessToken());
-    std::cout << "Device open\n";
-    if (device->StreamCount() == 0)
-    {
-        std::cout << "No stream available... Program exit.\n";
-        return 1;
-    }
+    triggerMode->SetValue("On");
+    triggerSource->SetValue("Software");
 
     EnablePacketResend(device);
     DiscardCorruptFrames(device);
+
     // get the first stream of the device
     auto stream = device->Stream();
+    // Increase buffer count, all images kept in RAM and written to memory after
+    stream->RingBuffer()->ChangeCount(buffers, Cvb::DeviceUpdateMode::UpdateDeviceImage);
+
     stream->Start();
     std::cout << "Acquisition started...\n";
 
+    // Triggering of cameras
+    while (stream->Statistics(Cvb::StreamInfo::NumBuffersPending) < buffers) {
+        triggerSoftware->Execute();
+    }
+
+    // Keep track of total images
     unsigned int count = 0;
+
+    std::cout << "processing frames\n";
+
+    // Now save images and free up the RAM
+    while (stream->Statistics(Cvb::StreamInfo::NumBuffersPending) > 0)
+    {
+        std::string padded_count = std::to_string(count);
+        padded_count.insert(0, 8 - padded_count.length(), '0');
+        std::string imagePath = imageDir + padded_count + ".bmp";
+        auto waitResult = stream->WaitFor(std::chrono::seconds(2));
+        if (waitResult.Status == Cvb::WaitStatus::Timeout)
+        {
+            throw std::runtime_error("acquisition timeout");
+        }
+        waitResult.Image->Save(imagePath);
+        std::cout << "Frames processed: " + std::to_string(count) + "\n";
+        count++;
+    }
+
 
     VideoProcessor recorder;
     recorder.setOutput("/data/test/vid.avi");
-
-    for (int i = 0; i < numImages; i++)
-    {
-        auto waitResult = stream->WaitFor(std::chrono::seconds(10));
-
-        if (waitResult.Status == Cvb::WaitStatus::Timeout)
-            {
-                throw std::runtime_error("acquisition timeout");
-            }
-        count++;
-        std::string padded_count = std::to_string(count);
-        padded_count.insert(0, 8 - padded_count.length(), '0');
-
-        std::string imagePath = imageDir + padded_count + ".bmp";
-
-        //waitResult.Image->Save(imagePath);
-        auto pImg = cv::Mat(waitResult.Image.height(), waitResult.Image.width(), CV_8UC3, waitResult.ImagePlane.Planes());
-        recorder.writeFrame(pImg);
-
-        if (count % 100 == 0)
-        {
-            std::cout << "Frames " + std::to_string(count) + "\n";
-
-        }
-    }
+    //auto pImg = cv::Mat(waitResult.Image->Height(), waitResult.Image->Width(), CV_8UC3, waitResult.ImagePlane.Planes());
+    //recorder.writeFrame(pImg);
 
     stream->Stop();
     std::cout << "Finished.\n";
