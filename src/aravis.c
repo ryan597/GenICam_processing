46f2217d0712f191a46813ad3bf837343508f5c8
Ryan Smith <ryan.smith@ucdconnect.ie>
2022-02-24 10:22:55 +0000

    fix lint errors and reformat

--- src/aravis.c
+++ src/aravis.c
@@ -1,34 +1,20 @@
-/* SPDX-License-Identifier:Unlicense */
-
-/* Aravis header */
-
-#include <arv.h>
-
-/* Standard headers */
-
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <arv.h>
 
 /*
 arv_camera_set_frame_rate()
 arv_camera_set_exposure_auto()
 arv_camera_set_gain_auto()
-
-
 */
-
-
-/*
- * Connect to the first available camera, then acquire 10 buffers.
- */
 
 int main (int argc, char **argv)
 {
     ArvCamera *camera;
     GError *error = NULL;
 
-    /* Connect to the first available camera */
+    // Connect to the first available camera
     camera = arv_camera_new (NULL, &error);
     arv_camera_gv_set_packet_size(camera, 8192, &error);
 
@@ -37,60 +23,57 @@ int main (int argc, char **argv)
 
         printf ("Found camera '%s'\n", arv_camera_get_model_name (camera, NULL));
 
-        /* Create the stream object without callback */
+        // Create the stream object without callback
         stream = arv_camera_create_stream (camera, NULL, NULL, &error);
         if (ARV_IS_STREAM (stream)) {
-            int i;
-            size_t payload;
-
-            /* Retrive the payload size for buffer creation */
-            payload = arv_camera_get_payload (camera, &error);
+            // Retrive the payload size for buffer creation
+            guint payload = arv_camera_get_payload (camera, &error);
             printf("Payload size '%lu'\n", payload);
             if (error == NULL) {
-                /* Insert some buffers in the stream buffer pool */
-                for (i = 0; i < 20; i++)
+                // Insert buffers in the stream buffer pool
+                for (int i = 0; i < 20; i++)
                     arv_stream_push_buffer (stream, arv_buffer_new (payload, NULL));
             }
 
             if (error == NULL)
             {
-                /* Start the acquisition */
+                // Start the acquisition
                 printf("Starting acquisition...\n");
                 arv_camera_start_acquisition (camera, &error);
             }
             if (error == NULL) {
-                /* Retrieve 10 buffers */
-                for (i = 0; i < 10; i++) {
+                // Retrieve 10 buffers
+                for (int i = 0; i < 10; i++) {
                     ArvBuffer *buffer;
                     printf("Frame %d ", i);
                     printf("HEre\n");
 
                     buffer = arv_stream_pop_buffer (stream);
                     if (ARV_IS_BUFFER (buffer)) {
-                        /* Display some informations about the retrieved buffer */
-                        printf ("Acquired %d×%d buffer\n",
+                        // Display some informations about the retrieved buffer
+                        printf ("Acquired %dx%d buffer\n",
                             arv_buffer_get_image_width (buffer),
                             arv_buffer_get_image_height (buffer));
-                        /* Don't destroy the buffer, but put it back into the buffer pool */
+                        // Don't destroy the buffer, but put it back into the buffer pool
                         arv_stream_push_buffer (stream, buffer);
                     }
                 }
             }
 
             if (error == NULL)
-                /* Stop the acquisition */
+                // Stop the acquisition
                 arv_camera_stop_acquisition (camera, &error);
 
-            /* Destroy the stream object */
+            // Destroy the stream object
             g_clear_object (&stream);
         }
 
-        /* Destroy the camera instance */
+        // Destroy the camera instance
         g_clear_object (&camera);
     }
 
     if (error != NULL) {
-        /* En error happened, display the correspdonding message */
+        // En error happened, display the correspdonding message
         printf ("Error: %s\n", error->message);
         return EXIT_FAILURE;
     }
