88c55f7a51e1aef95e211531c636ff4300d24c2b
Ryan Smith <ryan.smith@ucdconnect.ie>
2022-06-16 17:42:10 +0100

    finish check time function

---
+++ src/check_current_time.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <chrono>
+
+
+auto check_current_time(int minute_to_start) -> void
+{
+    // checks the time until it is minute_to_start:00:000
+    bool cond = true;
+    while (cond)
+    {
+        auto timenow = std::chrono::system_clock::now();
+        auto since_epoch = timenow.time_since_epoch();
+        auto hours = std::chrono::duration_cast<std::chrono::hours>(since_epoch);
+        since_epoch -= hours;
+        auto minutes = std::chrono::duration_cast<std::chrono::minutes>(since_epoch);
+        since_epoch -= minutes;
+        auto seconds = std::chrono::duration_cast<std::chrono::seconds>(since_epoch);
+        since_epoch -= seconds;
+        auto milli = std::chrono::duration_cast<std::chrono::milliseconds>(since_epoch);
+        since_epoch -= milli;
+        auto micro = std::chrono::duration_cast<std::chrono::microseconds>(since_epoch);
+
+        if (minutes.count() == minute_to_start && seconds.count() == 0 && milli.count() == 0)
+        {
+            cond = false;
+            std::cout << minutes.count() << ':' << seconds.count() << ':' << milli.count() << ':' << micro.count() << '\n';
+        }
+    }
+}
