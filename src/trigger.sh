0fec83a61c2cda7092f183ef4e6535e621c4a291
Ryan Smith <ryan.smith@ucdconnect.ie>
2023-02-22 14:34:30 +0000

    addition of cam0 and cam1

--- src/trigger.sh
+++ src/trigger.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 imageDir=`date +'%Y-%m-%d_%H-%M'`
-numImgs=100
-width=1500
-height=1000
+numImgs=1000
+width=2048
+height=2048
 framerate=10
 
 #saveDir=/run/media/pi/data/$imageDir/
-saveDir=../data/$imageDir/
+saveDir0=../data/0/$imageDir/
+saveDir1=../data/1/$imageDir/
+
+cam0="JAI Corporation-U507994"
+cam1="JAI Corporation-U507993"
+
 minute_to_start=$1
 
-mkdir -p $saveDir
+mkdir -p $saveDir0
+mkdir -p $saveDir1
 
-./build/main $numImgs $saveDir $width $height $framerate $minute_to_start 2>&1 | tee ../logs/$imageDir.txt
+./build/main $numImgs $saveDir0 $width $height $framerate $minute_to_start $pixel_format $cam0 &
+./build/main $numImgs $saveDir1 $width $height $framerate $minute_to_start $pixel_format $cam1
